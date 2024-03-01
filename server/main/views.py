98a72b8ae52a24f9f5a621e4a3cbbdec8546499d
Maxell Milay <maxell@bposeats.com>
2024-03-01 19:13:54 +0800

    [Maxell][Adjustment] removed node generation in backend

--- server/main/views.py
+++ server/main/views.py
@@ -27,44 +27,42 @@ class GenerateTopCareers(View):
 
         CAREER_TEMPLATE = """
         Use the following details below to recommend 5 most related career occupations with details.The format of the response should be strictly a JSON string of a list with elements following the exact format example below:
-        {"id":"Node 2","label":"Data Scientist","details":{"description":"sample description","salary":"100000 - 100000","companies":["Google","Oracle"],"qualifications":["python","web scraping"]},"group":2}
+        {"name":"Data Scientist","description":"sample description","salary":"100000 - 100000","companies":["Google","Oracle"],"qualifications":["python","web scraping"]}
         
         For the salary, let the currency be in Philippine Pesos (PHP).
-
-        Start the node id with 2, and increment one after each node. So the node numbers will be from 2 to 6. The groups of all the elements should be 2.
 
         Do not add anything to the output except what is asked by the prompt.
         """
 
-        CAREER_TEMPLATE += 'Career or topic interests:'
+        CAREER_TEMPLATE += 'My interests include:'
         if interest:
             for item in interest:
                 CAREER_TEMPLATE += '\n - ' + item
         else:
             CAREER_TEMPLATE += NO_ITEM
 
-        CAREER_TEMPLATE += 'Prior industry experiences:'
+        CAREER_TEMPLATE += 'My career history include:'
         if history:
             for item in history:
                 CAREER_TEMPLATE += '\n - ' + item
         else:
             CAREER_TEMPLATE += NO_ITEM
 
-        CAREER_TEMPLATE += 'Strengths:'
+        CAREER_TEMPLATE += 'My strengths include:'
         if strength:
             for item in strength:
                 CAREER_TEMPLATE += '\n - ' + item
         else:
             CAREER_TEMPLATE += NO_ITEM
 
-        CAREER_TEMPLATE += 'Weaknesses:'
+        CAREER_TEMPLATE += 'My wekanesses include:'
         if weakness:
             for item in weakness:
                 CAREER_TEMPLATE += '\n - ' + item
         else:
             CAREER_TEMPLATE += NO_ITEM
 
-        CAREER_TEMPLATE += 'Education background:'
+        CAREER_TEMPLATE += 'My educational background include:'
         if education:
             for item in education:
                 CAREER_TEMPLATE += '\n - ' + item
@@ -74,9 +72,9 @@ class GenerateTopCareers(View):
         CAREER_TEMPLATE += """
         You should also generate the following descriptions per career topic:
         1. brief description of the career (200 word limit)
-        2. salary range (example: 100,000 - 200,000)
+        2. salary range (example: 100,000 - 200,000 PHP)
         3. Top hiring companies (example: Google, Oracle)
-        4. Qualification List (example: java, python, web scraping)
+        4. Qualification List (example: java, linear algebra, web scraping)
 
         The response should only strictly be a list in the format as stated above. Do not include double or single quotes at the start or end, so reponse should only start with brackets. Do not add anything additional text except for the JSON string
         """
@@ -90,7 +88,7 @@ class GenerateTopCareers(View):
         print(response)
         answer = json.loads(response.content)
 
-        return JsonResponse({'response': answer})
+        return JsonResponse({'careers': answer})
 
 @method_decorator(csrf_exempt, name='dispatch')
 class GenerateUpskilling(View):
@@ -101,14 +99,12 @@ class GenerateUpskilling(View):
         UPSKILLING_TEMPLATE = "I want to know more about the career as a " 
         
         UPSKILLING_TEMPLATE += career + """
-        . First, I want to know exactly 2 main skills needed for my selected career. For each skill, I want exactly 2 specific suggestions as to where or how I can learn and develop it (example: website course, youtube video). I then want for each skill to know exactly a single way to verify or assess that certain skill that I learned (example: official certifications).
+        . First, I want to know at all main skills (as specific as possible) needed for my selected career, with description (200 word limit). For each skill, I want at all needed specific suggestions as to where or how I can learn and develop it (example: website course, youtube video), with description (200 word limit). I then want for each skill to know exactly a single way (as specific as possible) to verify or assess that certain skill that I learned (example: official certifications), with description (200 word limit).
 
-        sourceA and sourceB will be resources for skillA. sourceC and sourceD will be skillB. assessmentA will be for skillA and assessmentB will be for skillB. These are just placeholders for the data, do not put this as the response.
+        Strictly follow the format for the response. The format of the response should strictly be a list of elements following the format of a JSON string to be parsed like the following example below:
+        {"name":"skillName","description":"skillDescription","resources":[{"name":"resourceName","description":"resourceDescription","link":"resourceLink"},...],"assessment":{"name":"assessmentName","description":"assessmenetDescription","link":"assessmentLink"},...}
 
-        Strictly follow the format for the response. The format of the response should strictly be a JSON string to be parsed like the following example below:
-        [{"id":"Node 7","label":"skillA","details":{"description":"description of skillA"},"group":3},{"id":"Node 8","label":"skillB","details":{"description":"description of skillB"},"group":3},{"id":"Node 9","label":"sourceA","details":{"description":"description of sourceA"},"group":3},{"id":"Node 10","label":"sourceB","details":{"description":"description of sourceB"},"group":3},{"id":"Node 11","label":"sourceC","details":{"description":"description of sourceC"},"group":3},{"id":"Node 12","label":"sourceD","details":{"description":"description of sourceD"},"group":3},{"id":"Node 13","label":"assessmentA","details":{"description":"description of assessmentA"},"group":3},{"id":"Node 14","label":"assessmentB","details":{"description":"description of assessmentB"},"group":3}]
-        
-        The response should only strictly be in the JSON string format as stated above. Do not add anything additional text except for the JSON string in the response. Do not wrap the JSON string into extra quotations.
+        The response should only strictly be list of elements in a JSON string format as stated above. Do not add anything additional text except for the JSON string in the response. Do not wrap the JSON string into extra quotations.
         """
 
         langchain_messages = [
@@ -119,4 +115,4 @@ class GenerateUpskilling(View):
         response = chat.invoke(langchain_messages)
         answer = json.loads(response.content)
 
-        return JsonResponse({'response': answer})
+        return JsonResponse({'skills': answer})
